b2c2982dbdf06916bdc1632e8aded2093a240728
Byte-nana <nanadevelop44@gmail.com>
2025-09-11 15:49:22 +0100

    style(js-vanilla/1-alert): adjust selectors

--- js-vanilla/1-alert/main.js
+++ js-vanilla/1-alert/main.js
@@ -1,11 +1,11 @@
-const alert = document.querySelector('.alert:nth-child(1)');
-const icon = document.querySelector('i:nth-child(1)');
+const alert = document.querySelector('.alert:first-child');
+const offBtn = document.querySelector('.off__btn');
 const showBtn = document.querySelector('.show-btn');
 
 showBtn.addEventListener('click', () => {
   alert.style.display = 'block';
 });
 
-icon.addEventListener('click', () => {
+offBtn.addEventListener('click', () => {
   alert.style.display = 'none';
 });
