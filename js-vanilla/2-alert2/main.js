e4a9595830bfa25f5d38b2f687c4dd776b8a267e
Byte-nana <nanadevelop44@gmail.com>
2025-09-12 13:11:14 +0100

    refactor(js-vanilla/2-alert2):add function, event delegation

--- js-vanilla/2-alert2/main.js
+++ js-vanilla/2-alert2/main.js
@@ -1,24 +1,26 @@
 const showBtn = document.querySelector('.btn__wrapper');
-const closeBtn = document.querySelectorAll('.close__btn');
-const idAlert = document.querySelector('.id__alert');
-const pwAlert = document.querySelector('.pw__alert');
-const idBtn = document.querySelector('.id__btn');
-const pwBtn = document.querySelector('.pw__btn');
+const btns = document.querySelectorAll('.btn');
+
+const alertBox = document.querySelector('.alert');
+const closeIcon = document.querySelector('i');
+const alertM = document.querySelector('.alert__message');
+
+function updateAlert(bg, message) {
+  alertBox.style.display = 'block';
+  alertBox.style.background = bg;
+  alertM.innerHTML = message;
+}
 
 showBtn.addEventListener('click', (e) => {
-  if (e.target == idBtn) {
-    idAlert.style.display = 'block';
-  } else if (e.target == pwBtn) {
-    pwAlert.style.display = 'block';
+  if (e.target == btns[0]) {
+    updateAlert('#bae5c0', 'Enter your ID');
+  } else if (e.target == btns[1]) {
+    updateAlert('#e3c7e0', 'Enter your Passwords');
   }
 });
 
-closeBtn.forEach((btn, i) => {
-  btn.addEventListener('click', () => {
-    if (i === 0) {
-      idAlert.style.display = 'none';
-    } else if (i === 1) {
-      pwAlert.style.display = 'none';
-    }
-  });
+alertBox.addEventListener('click', (e) => {
+  e.target == closeIcon
+    ? (alertBox.style.display = 'none')
+    : (alertBox.style.display = 'block');
 });
