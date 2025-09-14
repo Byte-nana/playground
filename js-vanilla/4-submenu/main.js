e9b928def28310f0368a0849c1a4aff2a47f71e7
Byte-nana <nanadevelop44@gmail.com>
2025-09-14 23:28:08 +0100

    style(js-vanilla/4-submenu):add animation

--- js-vanilla/4-submenu/main.js
+++ js-vanilla/4-submenu/main.js
@@ -1,5 +1,5 @@
 const menuBtn = document.querySelector('.header__menu-btn');
-const submenu = document.querySelector('.submenu');
+const submenu = document.querySelector('.submenu__wrapper');
 
 menuBtn.addEventListener('click', () => {
   submenu.classList.toggle('submenu-show');
