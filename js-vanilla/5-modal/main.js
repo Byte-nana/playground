46f61c195b09a898a95d62117bd2a4aaa47081e4
Byte-nana <nanadevelop44@gmail.com>
2025-09-16 17:52:51 +0100

    feat(js-vamilla/5-modal):add regex email check

--- js-vanilla/5-modal/main.js
+++ js-vanilla/5-modal/main.js
@@ -9,3 +9,23 @@ showModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
   modal.classList.remove('pop-modal');
 });
+
+const form = document.querySelector('.modal__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputEmail = document.querySelector('#email');
+  const inputPw = document.querySelector('#password');
+  let email = inputEmail.value.trim();
+  let pw = inputPw.value.trim();
+  const regexEmail = /[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+/gm;
+
+  if (!email.match(regexEmail)) {
+    alert('📝 Enter your email in the proper form (e.g. name@email.com).');
+  } else if (pw.length < 6) {
+    alert('Passwords need more than 6 digits.');
+  } else {
+    alert('🎉 Login successful—welcome back!');
+  }
+});
