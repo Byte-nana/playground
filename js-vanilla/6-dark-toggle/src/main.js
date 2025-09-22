629a2dc02562b6b37075891a3fd57fb1f4163c87
Byte-nana <nanadevelop44@gmail.com>
2025-09-22 15:47:05 +0100

    feat(js-vanilla/6-dark-toggle):toggle dark/light mode

--- js-vanilla/6-dark-toggle/src/main.js
+++ js-vanilla/6-dark-toggle/src/main.js
@@ -1,10 +1,23 @@
 const checkbox = document.querySelector('#checkbox');
 const html = document.documentElement;
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
 
+// change into light theme
 checkbox.addEventListener('click', () => {
   if (checkbox.checked) {
     html.setAttribute('data-theme', 'light');
   } else {
     delete html.dataset.theme;
+  }
+});
+// sun and moon toggle
+checkbox.addEventListener('click', () => {
+  if (html.dataset.theme === 'light') {
+    sun.classList.add('show');
+    moon.classList.add('hidden');
+  } else {
+    sun.classList.remove('show');
+    moon.classList.remove('hidden');
   }
 });
